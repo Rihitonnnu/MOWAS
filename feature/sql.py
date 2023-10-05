32bf51e54023b9b4534178cefeeae063c7f798b3
Rihitonnnu <hexiliang04@gmail.com>
2023-10-05 16:29:10 +0900

    feat: sqlデータ取得

--- feature/sql.py
+++ feature/sql.py
@@ -5,22 +5,41 @@ class Sql:
     cnx = None
     cursor = None
 
-    def __init__():
+    def __init__(self):
         try:
-            cnx = mysql.connector.connect(
+            self.cnx = mysql.connector.connect(
                 user='user',  # ユーザー名
                 password='password',  # パスワード
-                host='localhost'  # ホスト名(IPアドレス）
+                host='localhost',  # ホスト名(IPアドレス）
+                database='mowas',
             )
 
-            if cnx.is_connected:
+            if self.cnx.is_connected:
                 print("Connected!")
 
         except Exception as e:
             print(f"Error Occurred: {e}")
 
     def select_name(self):
-        cursor = self.cnx.cursor()
-        query = '''select name from users'''
-        print(cursor)
-        # print('username is {}'.format(user_name))
+        try:
+            cursor = self.cnx.cursor()
+
+            query = '''
+                    SELECT  name 
+                    FROM    users
+                    '''
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            for row in rows:
+                print(row)
+        except:
+            pass
+        finally:
+            # クローズ
+            if cursor is not None:
+                cursor.close()
+            if self.cnx is not None and self.cnx.is_connected():
+                self.cnx.close()
+
+
+Sql().select_name()
