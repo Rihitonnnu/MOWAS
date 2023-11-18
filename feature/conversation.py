529fda91782b0a64bb1a0a66917e3c9761492360
Rihitonnnu <hexiliang04@gmail.com>
2023-11-18 22:24:49 +0900

    logの修正とmemoryの修正,tokenの記録の途中まで

--- feature/conversation.py
+++ feature/conversation.py
@@ -3,6 +3,7 @@ import logging
 from dotenv import load_dotenv
 import openai
 from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferWindowMemory
 from langchain.llms import OpenAI
 from langchain.chains import ConversationChain
 from langchain.prompts import ChatPromptTemplate
@@ -10,6 +11,7 @@ from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain.schema import BaseOutputParser
 from langchain.chat_models import ChatOpenAI
+from langchain.callbacks import get_openai_callback
 import logging
 from SyntheticVoice import SyntheticVoice
 from sql import Sql
@@ -17,19 +19,12 @@ import rec_unlimited
 from gpt import Gpt
 import beep
 import key_extraction
+import log_instance
 
 
 def conversation():
-    logger = logging.getLogger(__name__)
-    logger.setLevel(10)
-    sh = logging.StreamHandler()
-    logger.addHandler(sh)
-    fh = logging.FileHandler('../log/conversation.log',
-                             encoding='utf-8', mode='w')
-    logger.addHandler(fh)
-    formatter = logging.Formatter('%(message)s')
-    fh.setFormatter(formatter)
-    sh.setFormatter(formatter)
+    logger = log_instance.log_instance('conversation')
+    token_logger = log_instance.log_instance('token')
 
     load_dotenv()
 
@@ -73,8 +68,8 @@ def conversation():
     prompt = PromptTemplate(
         input_variables=["chat_history", "summary", "human_input"], template=template
     )
-    memory = ConversationBufferMemory(
-        memory_key="chat_history", input_key="human_input")
+    memory = ConversationBufferWindowMemory(
+        k=1, memory_key="chat_history", input_key="human_input")
 
     llm = ChatOpenAI(temperature=0.1)
     llm_chain = LLMChain(
@@ -84,44 +79,52 @@ def conversation():
         verbose=False
     )
 
-    # 事前に入力をしておくことでMOWAS側からの応答から会話が始まる
-    # 分岐はドライバーの名前が入力されているかどうか
-    if user_name != None:
-        response = llm_chain.predict(
-            human_input="こんにちは。あなたの名前はなんですか？私の名前は{}です。".format(user_name), summary=summary)
-    else:
-        response = llm_chain.predict(
-            human_input="こんにちは。あなたの名前はなんですか？名前の登録をしたいです")
-    syntheticVoice.speaking(response.replace('AI: ', '').replace('もわす: ', ''))
-    print(response.replace('AI: ', ''))
-
-    # 利用者が初めて発話、それに対する応答
-    human_input = rec_unlimited.recording_to_text()
-    # human_input = input("You: ")
-    logger.info(user_name + ": " + human_input)
-    response = llm_chain.predict(human_input=human_input, summary=summary)
-    logger.info(response.replace('AI: ', ''))
-    syntheticVoice.speaking(response.replace('AI: ', '').replace('もわす: ', ''))
-
-    human_input = rec_unlimited.recording_to_text()
-    # human_input = input("You: ")
-    logger.info(user_name + ": " + human_input)
-
-    while True:
-        try:
+    with get_openai_callback() as cb:
+        # 事前に入力をしておくことでMOWAS側からの応答から会話が始まる
+        # 分岐はドライバーの名前が入力されているかどうか
+        if user_name != None:
             response = llm_chain.predict(
-                human_input=human_input, summary=summary)
-            logger.info(response.replace('AI: ', ''))
-            syntheticVoice.speaking(response.replace(
-                'AI: ', '').replace('もわす: ', ''))
-            human_input = rec_unlimited.recording_to_text()
-            key_extraction(human_input)
-            # human_input = input("You: ")
-            logger.info(user_name + ": " + human_input)
-        except KeyboardInterrupt:
-            syntheticVoice.speaking("会話を終了しています。しばらくお待ち下さい ")
-            summary = Gpt().make_conversation_summary()
-            Sql().store_conversation_summary(summary)
-            Sql().store_conversation()
-            beep.high()
-            exit(1)
+                human_input="こんにちは。あなたの名前はなんですか？私の名前は{}です。".format(user_name), summary=summary)
+        else:
+            response = llm_chain.predict(
+                human_input="こんにちは。あなたの名前はなんですか？名前の登録をしたいです")
+        syntheticVoice.speaking(response.replace(
+            'AI: ', '').replace('もわす: ', ''))
+        print(response.replace('AI: ', ''))
+
+        token_logger(cb)
+        # exit(1)
+
+        # 利用者が初めて発話、それに対する応答
+        # human_input = rec_unlimited.recording_to_text()
+        human_input = input("You: ")
+        logger.info(user_name + ": " + human_input)
+        response = llm_chain.predict(human_input=human_input, summary=summary)
+        # token_logger(cb)
+        # print(cb)
+        logger.info(response.replace('AI: ', ''))
+        syntheticVoice.speaking(response.replace(
+            'AI: ', '').replace('もわす: ', ''))
+
+        # human_input = rec_unlimited.recording_to_text()
+        human_input = input("You: ")
+        logger.info(user_name + ": " + human_input)
+
+        while True:
+            try:
+                response = llm_chain.predict(
+                    human_input=human_input, summary=summary)
+                logger.info(response.replace('AI: ', ''))
+                syntheticVoice.speaking(response.replace(
+                    'AI: ', '').replace('もわす: ', ''))
+                # human_input = rec_unlimited.recording_to_text()
+                key_extraction(human_input)
+                human_input = input("You: ")
+                logger.info(user_name + ": " + human_input)
+            except KeyboardInterrupt:
+                syntheticVoice.speaking("会話を終了しています。しばらくお待ち下さい ")
+                summary = Gpt().make_conversation_summary()
+                Sql().store_conversation_summary(summary)
+                Sql().store_conversation()
+                beep.high()
+                exit(1)
