8fcc3b1479337bbe79fd533b13e30edec657bb5d
Rihitonnnu <hexiliang04@gmail.com>
2023-11-26 22:16:24 +0900

    実装できたがコストと実装面で問題あり

--- feature/search_spot.py
+++ feature/search_spot.py
@@ -7,27 +7,28 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-# ツールの準備
-tools = load_tools(["google-search"], llm=ChatOpenAI())
+def search_spot():
+    # ツールの準備
+    tools = load_tools(["google-search"], llm=ChatOpenAI())
 
-# プロンプトテンプレートの準備
-prefix = """次の質問にできる限り答えてください。次のツールにアクセスできます:"""
-suffix = """始めましょう! 
+    # プロンプトテンプレートの準備
+    prefix = """次の質問にできる限り答えてください。次のツールにアクセスできます:"""
+    suffix = """始めましょう! 
 
-Question: {input}
-{agent_scratchpad}"""
+    Question: {input}
+    {agent_scratchpad}"""
 
-prompt = ZeroShotAgent.create_prompt(
-    tools,
-    prefix=prefix,
-    suffix=suffix,
-    input_variables=["input", "agent_scratchpad"]
-)
+    prompt = ZeroShotAgent.create_prompt(
+        tools,
+        prefix=prefix,
+        suffix=suffix,
+        input_variables=["input", "agent_scratchpad"]
+    )
 
-# エージェントの準備
-llm_chain = LLMChain(llm=ChatOpenAI(temperature=0), prompt=prompt)
-agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools)
-agent_executor = AgentExecutor.from_agent_and_tools(
-    agent=agent, tools=tools, verbose=True)
+    # エージェントの準備
+    llm_chain = LLMChain(llm=ChatOpenAI(temperature=0), prompt=prompt)
+    agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools)
+    agent_executor = AgentExecutor.from_agent_and_tools(
+        agent=agent, tools=tools, verbose=True)
 
-agent_executor.run("ぼっち・ざ・ろっくの作者は誰だっけ？")
+    agent_executor.run("福岡県福岡市西区のカフェを一つ教えて")
