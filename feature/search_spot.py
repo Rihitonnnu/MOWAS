4b3f91f0854e80ecd097ca5e4c26485e92d1897d
Rihitonnnu <hexiliang04@gmail.com>
2023-12-03 21:00:55 +0900

    スポット検索機能の実装

---
+++ feature/search_spot.py
@@ -0,0 +1,40 @@
+import pprint
+import requests
+import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# APIのエンドポイントURL
+url = "https://places.googleapis.com/v1/places:searchNearby"
+
+# リクエストボディ
+payload = {
+    "includedTypes": ["restaurant"],
+    "maxResultCount": 2,
+    "locationRestriction": {
+        "circle": {
+            "center": {
+                "latitude": 37.7937,
+                "longitude": -122.3965},
+            "radius": 500.0
+        }
+    }
+}
+
+# ヘッダー
+headers = {
+    'Content-Type': 'application/json',
+    'X-Goog-Api-Key': os.environ["GOOGLE_API_KEY"],  # 実際のAPIキーに置き換えてください
+    'X-Goog-FieldMask': 'places.displayName',
+    'Accept-Language': 'ja'  # 日本語での結果を得るために追加
+}
+
+# POSTリクエストを実行
+response = requests.post(url, headers=headers, data=json.dumps(payload))
+
+# レスポンスのJSONを取得
+data = response.json()
+
+# 結果を表示（pprintモジュールを使用して見やすく表示）
+pprint.pprint(data)
