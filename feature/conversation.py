fafb4a233eda7b48c7da6faaa3b23efe07afcbf9
Rihitonnnu <hexiliang04@gmail.com>
2023-12-11 00:59:24 +0900

    embeddingの実装

--- feature/conversation.py
+++ feature/conversation.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 import openai
+import json
+import numpy as np
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
@@ -15,6 +17,8 @@ import log_instance
 from token_record import TokenRecord
 from search_spot import SearchSpot
 
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
 
 def conversation():
     # ログの設定
@@ -22,7 +26,6 @@ def conversation():
 
     # 環境変数読み込み
     load_dotenv()
-    openai.api_key = os.environ["OPENAI_API_KEY"]
 
     syntheticVoice = SyntheticVoice()
     token_record = TokenRecord()
@@ -145,3 +148,36 @@ def conversation():
 
             beep.high()
             exit(1)
+
+
+def embedding(input):
+    with open('json/index.json') as f:
+        INDEX = json.load(f)
+
+    # 入力を複数にしてqueryを用意してコサイン類似度を用いて検索させる
+    query = openai.Embedding.create(
+        model='text-embedding-ada-002',
+        input=input
+    )
+
+    query = query['data'][0]['embedding']
+
+    results = map(
+        lambda i: {
+            'body': i['body'],
+            # ここでクエリと各文章のコサイン類似度を計算
+            'similarity': cosine_similarity(i['embedding'], query)
+        },
+        INDEX
+    )
+    # コサイン類似度で降順（大きい順）にソート
+    results = sorted(results, key=lambda i: i['similarity'], reverse=True)
+
+    print(results)
+
+    # 類似性の高い選択肢を出力
+    print(f'一番近い文章は {results[0]["body"]} です')
+
+
+def cosine_similarity(a, b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
