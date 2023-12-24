19d0ebf5a5258c83807c20d6656bf03381f8276d
Rihitonnnu <hexiliang04@gmail.com>
2023-12-25 00:49:06 +0900

    不要なファイル削除

--- test/geological.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-geo_request_url = 'https://get.geojs.io/v1/ip/geo.json'
-data = requests.get(geo_request_url).json()
-print("緯度:", data['latitude'])
-print("経度:", data['longitude'])
