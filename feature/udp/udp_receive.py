bcedf0dacd31bfa6f5a2c6750e79e794faa3f2d6
Rihitonnnu <hexiliang04@gmail.com>
2023-12-25 17:43:02 +0900

    udpの受信機能を修正

--- feature/udp/udp_receive.py
+++ feature/udp/udp_receive.py
@@ -1,31 +1,49 @@
 import socket
 import struct
 
-# 自分のPCのアドレスとport.
-host = '127.0.0.1'
-port = 2002
-locaddr = (host, port)
+class UDPReceive():
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.locaddr = (host, port)
+        self.sock = socket.socket(socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.sock.bind(self.locaddr)
+        print('create socket')
 
-# ソケットを作成する
-sock = socket.socket(socket.AF_INET, type=socket.SOCK_DGRAM)
-print('create socket')
+    def get_eor(self):
+        try :
+            # 受付待ち
+            print('Waiting data...')
+            data, cli_addr = self.sock.recvfrom(1024)
 
-# 自ホストで使用するIPアドレスとポート番号を指定
-sock.bind(locaddr)
+            # dataを浮動小数点数に変換
+            data = struct.unpack('d', data)[0]
+            print('Received data: {}'.format(data))
+            return data
+        except KeyboardInterrupt:
+            print ('\\n . . .\\n')
+            self.sock.close()
+            return None
 
-while True:
-    try :
-        # 受付待ち
-        print('Waiting data...')
-        data, cli_addr = sock.recvfrom(1024)
+    def get_coordinates(self):
+        try :
+            # 受付待ち
+            print('Waiting data...')
+            data, cli_addr = self.sock.recvfrom(1024)
 
-        coordinates_result=[]
-        for i in range(0, len(data), 8):
-            num = struct.unpack('d', data[i:i+8])[0]
-            coordinates_result.append(num)
-        
-        print('Received data: {}'.format(coordinates_result))
-    except KeyboardInterrupt:
-        print ('\\n . . .\\n')
-        sock.close()
-        break
+            coordinates_result=[]
+            for i in range(0, len(data), 8):
+                num = struct.unpack('d', data[i:i+8])[0]
+                coordinates_result.append(num)
+            
+            print('Received data: {}'.format(coordinates_result))
+            return coordinates_result
+        except KeyboardInterrupt:
+            print ('\\n . . .\\n')
+            self.sock.close()
+            return None
+
+    def close(self):
+        self.sock.close()
+
+UDPReceive('127.0.0.1',2002).get_eor()
