67bec08c885917011f37256373e6ece1caded2cf
Rihitonnnu <hexiliang04@gmail.com>
2024-01-01 22:46:14 +0900

    wip: main修正

--- feature/main.py
+++ feature/main.py
@@ -2,6 +2,7 @@
 import conversation
 import os
 import view.option_window
+import udp.udp_receive
 
 try:
     os.mkdir('../sound')
@@ -13,4 +14,12 @@ try:
 except FileExistsError:
     pass
 
+# 眠くなりかけるまで待機
+while True:
+    is_sleepy=udp.udp_receive.UDPReceive('127.0.0.1',2002).is_sleepy()
+
+    if is_sleepy:
+        break
+
+exit(1)
 conversation.conversation()
