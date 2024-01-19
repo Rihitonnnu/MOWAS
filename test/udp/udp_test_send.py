b5f07aca6a7dc0bb1175f8f6f31b71e6446d7196
Rihitonnnu <hexiliang04@gmail.com>
2024-01-20 01:57:51 +0900

    テスト修正

--- test/udp/udp_test_send.py
+++ test/udp/udp_test_send.py
@@ -5,8 +5,15 @@ load_dotenv()
 
 
 def udp_client(message, server_ip, server_port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(message.encode(), (server_ip, server_port))
+    while True:
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.sendto(message.encode(), (server_ip, server_port))
+            sock.close()
+        except KeyboardInterrupt:
+            break
+        except Exception as e:
+            break
 
-
-udp_client("Hello, Server!", os.environ["SCHOOL_MACHINE_IP"], 12345)
+udp_client("Hello, Server!", '127.0.0.1', 12345)
+# udp_client("Hello, Server!", os.environ["SCHOOL_MACHINE_IP"], 12345)
