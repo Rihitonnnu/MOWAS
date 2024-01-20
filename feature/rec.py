5b8c38e865c0d8f240e52f716fd3249a38f6a100
Rihitonnnu <hexiliang04@gmail.com>
2024-01-20 16:53:06 +0900

    fix: rec

--- feature/rec.py
+++ feature/rec.py
@@ -2,28 +2,34 @@ import speech_recognition as sr
 import beep
 from SyntheticVoice import SyntheticVoice
 
+listener=sr.Recognizer()
+
 class Rec:
     def __init__(self):
-        self.listener = sr.Recognizer()
         self.rec_cnt=0
 
     def run(self):
-        try:
-            with sr.Microphone() as source:
-                # beep.high()
-                voice = self.listener.listen(source)
-                human_input = self.listener.recognize_google(voice,language="ja-JP")
-                beep.low()
-                print(human_input)
-                return human_input
-        except:
-            print('聞き取れませんでした')
-            if self.rec_cnt<1:
-                SyntheticVoice().speaking('すみません、聞き取れませんでした。ビープ音の後にもう一度お願いします。')
+        for _ in range(2):
+            try:
+                with sr.Microphone() as source:
+                    # beep.high()
+                    voice = listener.listen(source)
+                    human_input = listener.recognize_google(voice,language="ja-JP")
+                    beep.low()
+                    print(human_input)
+                    return human_input
+            except:
+                print('聞き取れませんでした')
                 self.rec_cnt+=1
-                beep.high()
-                self.run()
 
-            return None
+                if self.rec_cnt<2:
+                    # SyntheticVoice().speaking('すみません、聞き取れませんでした。ビープ音の後にもう一度お願いします。')
+                    self.rec_cnt+=1
+                    beep.high()
+                else:
+                    return None
+            
+            else:
+                break
 
 # Rec().run()
