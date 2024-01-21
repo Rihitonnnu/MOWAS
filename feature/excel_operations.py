df76c4ed8a9c6fc3f7a6bded8aa1d3a417c4015d
Rihitonnnu <hexiliang04@gmail.com>
2024-01-21 14:29:24 +0900

    fix: excel

--- feature/excel_operations.py
+++ feature/excel_operations.py
@@ -1,11 +1,12 @@
 import openpyxl
+from datetime import datetime
 
 class ExcelOperations:
     def __init__(self, reaction_time_sheet_path):
         self.reaction_time_sheet_path = reaction_time_sheet_path
 
     # 反応時間を記録する
-    def rac_time_excel(self, start_time, end_time):
+    def rac_time_excel(self, start_time, end_time,conv_start_time):
         # excelシートを読み込む
         wb = openpyxl.load_workbook(self.reaction_time_sheet_path)
         sheet = wb.active
@@ -15,8 +16,8 @@ class ExcelOperations:
         sheet.cell(row=last_row + 1, column=1, value=last_row)
         # reaction_timeカラムに書き込む
         sheet.cell(row=last_row + 1, column=2, value=(end_time-start_time).total_seconds())
-        # measurement_timeにself.conv_start_timeとself.end_timeの差分を%M:%s形式で書き込む
-        # sheet.cell(row=last_row + 1, column=3, value=(self.conv_start_time-self.end_time))
+        # measurement_timeにself.conv_start_timeとself.end_timeの差分を書き込む
+        sheet.cell(row=last_row + 1, column=3, value=(end_time-conv_start_time))
 
         print(end_time-start_time)
         # 保存
