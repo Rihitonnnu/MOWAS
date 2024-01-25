9eec0186ba301523731d2f11dc8f7225dcef8339
Rihitonnnu <hexiliang04@gmail.com>
2024-01-25 14:20:11 +0900

    fix: demo

--- feature/demo.py
+++ feature/demo.py
@@ -4,6 +4,7 @@ from rec import Rec
 import os
 import time
 import beep
+import datetime
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -14,15 +15,29 @@ class Demo:
         self.udp_receive = UDPReceive(os.environ['MATSUKI7_IP'], 12345)
         self.rec = Rec()
 
-    def run(self):
-        self.synthetic_voice.speaking('こんにちは。デモを始めます')
-        time.sleep(0.5)
+    # 反応時間計測
+    def rac_time_measure(self):
+        # 開始時間の取得及び変換
         beep.high()
+        self.start_time = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S.%f')
+        self.start_time = datetime.datetime.strptime(self.start_time, '%Y/%m/%d %H:%M:%S.%f')
+
+        while True:
+            date=None
+            date=self.udp_receive.get_end_time()
+            if date is not None:
+                # dateを日付型に変換
+                self.end_time=datetime.datetime.strptime(date, '%Y/%m/%d %H:%M:%S.%f')
+                break
+
+    def run(self):
+        self.synthetic_voice.speaking('デモを始めます')
         # 反応時間計測
         self.rac_time_measure()
+
         for _ in range(2):
             self.human_input = self.rec.run()
             if isinstance(self.human_input,str):
                 break
-
+        
 Demo().run()
