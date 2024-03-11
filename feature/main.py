7d9ac838b34af8684943be59530878275608c4ca
Rihitonnnu <hexiliang04@gmail.com>
2024-03-11 17:41:03 +0900

    fix

--- feature/main.py
+++ feature/main.py
@@ -13,16 +13,10 @@ now=datetime.datetime.now()
 ymd=now.strftime('%Y%m%d')
 hms=now.strftime('%H%M%S')
 
-# 名前を入力
+# 名前を入力とMOWASを使用するかどうか
 name='kawanishi'
-mowas_use=False
+mowas_use=True
 
-
-# ディレクトリやファイル作成
-# try:
-#     os.makedirs('../sound/{}'.format(now.strftime('%Y%m%d')))
-# except FileExistsError:
-#     pass
 try:
     os.makedirs('../log')
 except FileExistsError:
@@ -44,16 +38,16 @@ sheet['C1'] = 'measurement_time'
 sheet['D1'] = 'guide_acc_time'
 
 # 眠くなりかけるまで待機
-# while True:
-#     try:
-#         is_sleepy=udp.udp_receive.UDPReceive(os.environ['MATSUKI7_IP'],12345).is_sleepy()
-#         print(is_sleepy)
-#         if is_sleepy:
-#             # beep音を鳴らす
-#             beep.high()
-#             break
-#     except Exception as e:
-#         pass
+while True:
+    try:
+        is_sleepy=udp.udp_receive.UDPReceive(os.environ['MATSUKI7_IP'],12345).is_sleepy()
+        print(is_sleepy)
+        if is_sleepy:
+            # beep音を鳴らす
+            beep.high()
+            break
+    except Exception as e:
+        pass
 
 # MOWAS使用
 if mowas_use:
